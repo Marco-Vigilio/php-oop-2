5ec4dc2eae4c2d8c716ed29feefbe7565f975d36
Marco-Vigilio <tonyvigilio@gmail.com>
2023-07-14 16:43:32 +0200

    visualizzo il prodotto con la categoria

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+
+class Prodotti extends Categorie{
+    public $immagine;
+    public $titolo;
+    public $prezzo;
+    public $categoria;
+
+    public function __construct(string $_immagine, string $_titolo, string $_prezzo, Categorie $_categoria ){
+        $this->immagine = $_immagine;
+        $this->titolo = $_titolo;
+        $this->prezzo = $_prezzo;
+        $this->categoria = $_categoria;
+    }
+}
+
+class Categorie {
+    public $nomeCategoria;
+
+    public function __construct(string $_nomeCategoria){
+        $this->nomeCategoria = $_nomeCategoria;
+    }
+}
+
+class Articoli {
+
+}
+
+$categoria = [
+    $cani = new Categorie("cani"),
+    $gatti = new Categorie("gatti"),
+];
+
+
+
+$prod = new Prodotti("qualcosa", "Osso di gomma", "2€", $categoria[0]);
+var_dump($prod);
+
+?>
