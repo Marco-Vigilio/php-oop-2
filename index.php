a843225f1ea874861832666c307ba17dd5e3fa00
Marco-Vigilio <tonyvigilio@gmail.com>
2023-07-14 18:32:39 +0200

    stilizzo la pagina inserendo i miei prodotti

--- index.php
+++ index.php
@@ -4,15 +4,17 @@ class Prodotti {
     public $immagine;
     public $titolo;
     public $prezzo;
-    public $categoria;
+    
     public $tipologia;
+    public $categoria;
 
-    public function __construct(string $_immagine, string $_titolo, string $_prezzo, Categorie $_categoria, Tipologie $_tipologia ){
+    public function __construct(string $_immagine, string $_titolo, string $_prezzo,  Categorie $_categoria, Tipologie $_tipologia,  ){
         $this->immagine = $_immagine;
         $this->titolo = $_titolo;
         $this->prezzo = $_prezzo;
-        $this->categoria = $_categoria;
+        
         $this->tipologia = $_tipologia;
+        $this->categoria = $_categoria;
     }
 }
 
@@ -26,11 +28,11 @@ class Categorie{
     }
 }
 
-class Tipologie {
+class Tipologie{
     public $nomeTipologia;
 
     public function __construct(string $_nomeTipologia){
-        $this->tipologia = $_nomeTipologia;
+        $this->nomeTipologia = $_nomeTipologia;
     }
 }
 
@@ -48,8 +50,12 @@ $tipologie = [
 
 
 $prodotti = [
-    new Prodotti("qualcosa", "Osso di gomma", "2€", $categorie[0], $tipologie[2]),
-    new Prodotti("qualcosa", "Ciondolo per gatti", "4€", $categorie[1], $tipologie[0]),
+    new Prodotti("https://source.unsplash.com/random/250x400?sig=".rand(0,100), "Osso di gomma", "2€", $categorie[0], $tipologie[2]),
+    new Prodotti("https://source.unsplash.com/random/250x400?sig=".rand(0,100), "Ciondolo per gatti",  "4€", $categorie[1], $tipologie[0]),
+    new Prodotti("https://source.unsplash.com/random/250x400?sig=".rand(0,100), "Tonno per gatti", "4€", $categorie[1], $tipologie[1]),
+    new Prodotti("https://source.unsplash.com/random/250x400?sig=".rand(0,100), "Osso di gomma", "2€", $categorie[0], $tipologie[2]),
+    new Prodotti("https://source.unsplash.com/random/250x400?sig=".rand(0,100), "Ciondolo per gatti",  "4€", $categorie[1], $tipologie[0]),
+    new Prodotti("https://source.unsplash.com/random/250x400?sig=".rand(0,100), "Tonno per gatti", "4€", $categorie[1], $tipologie[1]),
 ];
 
 ?>
@@ -60,6 +66,8 @@ $prodotti = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css">
     <title>Shop Animals</title>
 </head>
 <body>
@@ -69,9 +77,17 @@ $prodotti = [
         </h1>
         <div class="items">
         <?php foreach ($prodotti as $prodotto) { ?>
-            <div class="card col-3 p-0 m-3" >
-                <div class="card-header">
-                    <?php echo $prodotto->categoria->nomeCategoria; ?>
+            <div class="card">
+                <div class="card_header">
+                    <h2><?php echo $prodotto->tipologia->nomeTipologia;?></h2>
+                    <i class="<?php echo $prodotto->categoria->icon; ?>"></i>
+                </div>
+
+                    <img src="<?php echo $prodotto->immagine; ?>" alt="<?php echo $prodotto->titolo; ?>">
+
+                <div class="info">
+                    <h3><?php echo $prodotto->titolo; ?></h3>
+                    <p>Prezzo: <?php echo $prodotto->prezzo; ?></p>
                 </div>
                 
             </div>
@@ -80,23 +96,3 @@ $prodotti = [
     </div>
 </body>
 </html>
-
-<!--
-                <img src="<?php echo $post->image ?>"  class="card-img-top rounded-0" alt="<?php echo $post->title; ?>">
-                <div class="card-body p-3">
-                    <h5 class="card-title">
-                    <?php echo $post->title; ?>
-                    </h5>
-                    <h6 class="card-subtitle mb-2 text-muted">
-                        <?php echo $post->author->username; ?>
-                        <br>
-                        <?php echo $post->dateTime->format('Y-m-d H:i:s'); ?>
-                    </h6>
-                    <p class="card-text">
-                        <?php echo $post->getSynopsis(88); ?>
-                    </p>
-                    <a href="#" class="btn btn-primary">
-                        Read more...
-                    </a>
-                </div>
-                -->
