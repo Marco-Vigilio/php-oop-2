859a07b6bf2417b8d8d7ce3d2300dfea7926028b
Marco-Vigilio <tonyvigilio@gmail.com>
2023-07-17 17:25:03 +0200

    inserisco le sottoclassi figlie Giocattolo e Cibo

--- index.php
+++ index.php
@@ -36,6 +36,47 @@ class Tipologie{
     }
 }
 
+class Giocattolo extends Prodotti{
+    public $grandezza;
+    public $materiale;
+
+    public function __construct(string $_immagine, string $_titolo, string $_prezzo,  Categorie $_categoria, Tipologie $_tipologia, string $_grandezza, string $_materiale){
+        parent::__construct( $_immagine,  $_titolo,  $_prezzo,   $_categoria,  $_tipologia);
+        $this->grandezza = $_grandezza;
+        $this->materiale = $_materiale;
+    }
+
+
+}
+
+class Cibo extends Prodotti{
+    public $prodotto_il;
+    public $scadenza;
+    public $calorie;
+    public $grassi;
+
+    public function __construct(string $_immagine, string $_titolo, string $_prezzo,  Categorie $_categoria, Tipologie $_tipologia, string $_prodotto_il, string $_scadenza, string $_calorie, string $_grassi){
+        parent::__construct( $_immagine,  $_titolo,  $_prezzo,   $_categoria,  $_tipologia);
+        $this->prodotto_il = $_prodotto_il;
+        $this->scadenza = $_scadenza;
+        //$this->scadenza = getDataScadenza($_prodotto_il);
+        $this->calorie = $_calorie;
+        $this->grassi = $_grassi;
+    }
+
+    public getDataProduzione( string $_prodotto_il) : string{
+        $date=date_create($_prodotto_il);
+        return $date;
+    }
+    /*
+    public getDataScadenza ($_prodotto_il){
+        $date=date_create($_prodotto_il);
+        date_add($date,date_interval_create_from_date_string("40 days"));
+        return date_format($date,"d-m-Y");
+    }
+    */
+}
+
 $categorie = [
     new Categorie("Cani", "fa-solid fa-dog"),
     new Categorie("Gatti", "fa-solid fa-cat"),
@@ -50,12 +91,15 @@ $tipologie = [
 
 
 $prodotti = [
-    new Prodotti("https://source.unsplash.com/random/250x400?sig=".rand(0,100), "Osso di gomma", "2€", $categorie[0], $tipologie[2]),
-    new Prodotti("https://source.unsplash.com/random/250x400?sig=".rand(0,100), "Ciondolo per gatti",  "4€", $categorie[1], $tipologie[0]),
-    new Prodotti("https://source.unsplash.com/random/250x400?sig=".rand(0,100), "Tonno per gatti", "4€", $categorie[1], $tipologie[1]),
-    new Prodotti("https://source.unsplash.com/random/250x400?sig=".rand(0,100), "Osso di gomma", "2€", $categorie[0], $tipologie[2]),
-    new Prodotti("https://source.unsplash.com/random/250x400?sig=".rand(0,100), "Ciondolo per gatti",  "4€", $categorie[1], $tipologie[0]),
-    new Prodotti("https://source.unsplash.com/random/250x400?sig=".rand(0,100), "Tonno per gatti", "4€", $categorie[1], $tipologie[1]),
+    new Prodotti("https://source.unsplash.com/random/250x200?sig=".rand(0,100), "Osso di gomma", "2€", $categorie[0], $tipologie[2]),
+    new Prodotti("https://source.unsplash.com/random/250x200?sig=".rand(0,100), "Ciondolo per gatti",  "4€", $categorie[1], $tipologie[0]),
+    new Prodotti("https://source.unsplash.com/random/250x200?sig=".rand(0,100), "Tonno per gatti", "4€", $categorie[1], $tipologie[1]),
+    new Prodotti("https://source.unsplash.com/random/250x200?sig=".rand(0,100), "Osso di gomma", "2€", $categorie[0], $tipologie[2]),
+    new Prodotti("https://source.unsplash.com/random/250x200?sig=".rand(0,100), "Ciondolo per gatti",  "4€", $categorie[1], $tipologie[0]),
+    new Prodotti("https://source.unsplash.com/random/250x200?sig=".rand(0,100), "Tonno per gatti", "4€", $categorie[1], $tipologie[1]),
+
+    new Giocattolo("https://source.unsplash.com/random/250x200?sig=".rand(0,100), "Osso di gomma", "2€", $categorie[0], $tipologie[2], "medium", "gomma"),
+    new Cibo("https://source.unsplash.com/random/250x200?sig=".rand(0,100), "Tonno per gatti", "4€", $categorie[1], $tipologie[1], "2013-03-15", "2013-03-20", "20%", "40%"),
 ];
 
 ?>
@@ -85,9 +129,26 @@ $prodotti = [
 
                     <img src="<?php echo $prodotto->immagine; ?>" alt="<?php echo $prodotto->titolo; ?>">
 
-                <div class="info">
+                <div class="info p-3">
                     <h3><?php echo $prodotto->titolo; ?></h3>
                     <p>Prezzo: <?php echo $prodotto->prezzo; ?></p>
+
+                    <?php if(get_class($prodotto) === "Giocattolo"){?>
+                    <div >
+                        <p>Materiale: <?php echo $prodotto->materiale;?></p>
+                        <p>Grandezza: <?php echo $prodotto->grandezza?></p>
+                    </div>
+                    <?php }elseif(get_class($prodotto) === "Cibo"){?>
+                    <div >
+                        <div class="d-flex justify-content-between">
+                            <p>Calorie: <?php echo $prodotto->calorie;?></p>
+                            <p>Grassi: <?php echo $prodotto->grassi;?></p>
+                        </div>
+                        
+                        <p>Prodotto il: <?php echo $prodotto->prodotto_il;?></p>
+                        <p>Da consumarsi entro: <?php echo $prodotto->scadenza?></p>
+                    </div>
+                    <?php }?>
                 </div>
                 
             </div>
